42ee0e2927fa078ab070dfdb439109fdf499b06f
jajamixster <sidejajamixster@gmail.com>
2020-04-10 02:42:25 +0700

    Initial commit

---
+++ src/main/java/com/mission1/HelloworldspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mission1/HelloworldspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mission1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
