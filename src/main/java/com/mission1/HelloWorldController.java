f4f082d2cbb14ef31746e5b057524b2cb9cf1225
jajamixster <sidejajamixster@gmail.com>
2020-04-10 19:27:26 +0700

    Change @RequestMapping Structure

--- src/main/java/com/mission1/HelloWorldController.java
+++ src/main/java/com/mission1/HelloWorldController.java
@@ -3,14 +3,14 @@ package com.mission1;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
-//set api path to /api/hello-world
-@RequestMapping("/api/hello-world")
-
 //mark class as Spring MVC REST Controller - this one combine the @Controller and @ResponseBody together
 @RestController
 
+//set api path /api for HelloWorldController
+@RequestMapping("/api")
 public class HelloWorldController {
-
+    //set api path /hello-world for getHello()
+    @RequestMapping("/hello-world")
     // HTTP GET Mapping
     @GetMapping
     public String getHello() {
@@ -19,6 +19,9 @@ public class HelloWorldController {
 
     // HTTP POST Mapping
     /* below are code for POST mapping
+    //set api path /hello-word for postHello()
+    @RequestMapping("/hello-world")
+    //HTTP POST Mapping
     @PostMapping
     public String postHello() {
         return "Hello World! What am I doing here :3 - This one is for POST endpoint";
