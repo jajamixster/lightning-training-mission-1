03ea13eeef8daa63fcc0796980d79e6186180360
Kittinun Boonyapruk <sidejajamixster@gmail.com>
2020-04-10 15:35:34 +0700

    Remove @ResponseBody

--- src/main/java/com/mission1/HelloWorldController.java
+++ src/main/java/com/mission1/HelloWorldController.java
@@ -4,18 +4,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/hello-world")
-//mark class as Spring MVC Controller
+//mark class as Spring MVC REST Controller - this one combine the @Controller and @ResponseBody together
 @RestController
 public class HelloWorldController {
     // HTTP GET Mapping
     @GetMapping
-    @ResponseBody
     public String getHello() {
         return "Hello World! What am I doing here :3 - This one is for GET endpoint";
     }
     // HTTP POST Mapping
     @PostMapping
-    @ResponseBody
     public String postHelllo() {
         return "Hello World! What am I doing here :3 - This one is for POST endpoint";
     }
