3fb5221ee810f718c0669d7d2d51852dfdb709a3
jajamixster <sidejajamixster@gmail.com>
2020-04-10 16:40:57 +0700

    remove POST endpoint

--- src/main/java/com/mission1/HelloWorldController.java
+++ src/main/java/com/mission1/HelloWorldController.java
@@ -3,18 +3,24 @@ package com.mission1;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
+//set api path to /api/hello-world
 @RequestMapping("/api/hello-world")
+
 //mark class as Spring MVC REST Controller - this one combine the @Controller and @ResponseBody together
 @RestController
+
 public class HelloWorldController {
+
     // HTTP GET Mapping
     @GetMapping
     public String getHello() {
         return "Hello World! What am I doing here :3 - This one is for GET endpoint";
     }
+
     // HTTP POST Mapping
+    /* below are code for POST mapping
     @PostMapping
-    public String postHelllo() {
+    public String postHello() {
         return "Hello World! What am I doing here :3 - This one is for POST endpoint";
-    }
+    }*/
 }
