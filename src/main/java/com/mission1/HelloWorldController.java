fc9a44e8c75e8965939b3879c6b190d55bef0098
jajamixster <sidejajamixster@gmail.com>
2020-04-13 10:04:46 +0700

    Fixing format

--- src/main/java/com/mission1/HelloWorldController.java
+++ src/main/java/com/mission1/HelloWorldController.java
@@ -9,6 +9,7 @@ public class HelloWorldController {
     //HTTP GET Mapping
     @GetMapping("/hello-world")
     public String getHello() {
+        //return String
         return "Hello World! What am I doing here :3 - This one is for GET endpoint";
     }
 }
